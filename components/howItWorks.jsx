05d3d1b5556e73048c6b3469ad38e72dcd81b475
Theophilus Mensah <unique.theo98@gmail.com>
2024-08-19 21:29:46 +0000

    Feat: How it works section

--- components/howItWorks.jsx
+++ components/howItWorks.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React from "react";
+import { AiOutlineRobot, AiOutlineUserAdd } from "react-icons/ai";
+import { HiOutlineClipboardCheck } from "react-icons/hi";
+import { MdQuestionAnswer } from "react-icons/md";
+import { FaStar } from "react-icons/fa";
+
+const steps = [
+  {
+    title: "Create Account",
+    description: "Setup an account with Career Coach",
+    icon: <AiOutlineUserAdd />,
+  },
+  {
+    title: "Choose a Job/Role",
+    description: "Choose a job role that you qualify the most for",
+    icon: <HiOutlineClipboardCheck />,
+  },
+  {
+    title: "AI Generated CV and Cover Letter",
+    description:
+      "A cover letter and CV is genrated from the job of choice and your skills",
+    icon: <AiOutlineRobot />,
+  },
+  {
+    title: "Mock Interview and Analyses",
+    description:
+      "Undertake a mock interview to groom you for the Job Interview",
+    icon: <MdQuestionAnswer />,
+  },
+  {
+    title: "Get Dream Job",
+    description:
+      "Share helpful resources on new role and coach you through your career",
+    icon: <FaStar />,
+  },
+  {
+    title: "Get Trained for Promotions",
+    description:
+      "Get training from professional in your field to on career growth paths",
+    icon: <FaStar />,
+  },
+];
 
 const HowItWorks = () => {
   return (
-    <div className=''>HowItWorks</div>
-  )
-}
+    <div className="max-w-[1440px] mx-auto py-12">
+      <div className="">
+        <div className="text-3xl text-center w-full mb-12 font-semibold leading-none tracking-tight ">
+          <h2>How it Works</h2>
+        </div>
+        <div className="grid grid-cols-3 w-full justify-between gap-4 text-center my-6">
+          {steps.map((step, id) => {
+            return (
+              <div
+                key={id}
+                className={`flex flex-col items-center gap-4 p-6 mb-8 rounded-md w-[450px] ${
+                  id % 2 ? "bg-white shadow" : ""
+                }`}
+              >
+                <div className="icon text-[100px]">{step.icon}</div>
+                <div className="text-2xl font-semibold leading-none tracking-tight">
+                  {step.title}
+                </div>
+                <div>{step.description}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default HowItWorks
+export default HowItWorks;
--- components/popularVacancies.jsx
+++ components/popularVacancies.jsx
@@ -18,7 +18,7 @@ const PopularVacancies = () => {
   return (
     <div className="max-w-[1440px] mx-auto py-12">
       <div className="mb-8">
-        <h3 className="text-2xl">Most Popular Vacancies</h3>
+        <h3 className="text-3xl text-center font-semibold leading-none tracking-tight">Most Popular Vacancies</h3>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         {popularVacancies.slice(0, 12).map((item, id) => {
